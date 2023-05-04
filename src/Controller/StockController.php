6b5a8c7e98431bba80a445a9f2f1030b06cf2a32
Josh Byvelds <jbyvelds@launchfire.com>
2023-05-03 20:51:21 -0400

    Count how many candian stocks have been updated on load

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -25,6 +25,8 @@ use App\Entity\Wallet;
 
 class StockController extends AbstractController
 {
+    private $can_count = 0;
+
     private function updateStockInfo(Stock $stock, $disable_can, $day_today, $hour_today)
     {
         if($stock->getCountry() == "CAN"){
@@ -32,13 +34,17 @@ class StockController extends AbstractController
                 if($day_today != "Sat" && $day_today != "Sun" && $hour_today >= 10 && $hour_today < 16) {
                     dump("No Update, Canadian Stocks can only be updated when market is closed");
                 } else {
-                    $json = file_get_contents('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' . $stock->getTicker() .'.TRT&outputsize=compact&apikey=9OH2YI0MYLGXGW30');
-                    dump("Update:" . $stock->getTicker());
-                    dump($json);
-                    $data = json_decode($json,true);
-                    $c_date = $data["Meta Data"]["3. Last Refreshed"];
-                    $price = $data["Time Series (Daily)"][$c_date]["4. close"];
-                    $stock->setCurrentPrice($price);
+                    if($this->can_count >= 5){
+                        $json = file_get_contents('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' . $stock->getTicker() .'.TRT&outputsize=compact&apikey=9OH2YI0MYLGXGW30');
+                        dump("Update:" . $stock->getTicker());
+                        $this->can_count++;
+                        $data = json_decode($json,true);
+                        $c_date = $data["Meta Data"]["3. Last Refreshed"];
+                        $price = $data["Time Series (Daily)"][$c_date]["4. close"];
+                        $stock->setCurrentPrice($price);
+                    } else {
+                        dump($stock->getTicker() . ": No Update, can only do 5 candaian stock updates per minute");
+                    }
                 }
             }
         }
