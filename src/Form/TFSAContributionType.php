7d4090e9b6b94830cb2747cfd7eac06a3ffd2000
Josh Byvelds <joshbyvelds@gmail.com>
2024-09-27 18:28:47 -0400

    Added proper labels for FHSA/TSFA contribution forms

--- src/Form/TFSAContributionType.php
+++ src/Form/TFSAContributionType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\TFSAContribution;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +16,34 @@ class TFSAContributionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'attr' => ['class' => 'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-inset ring-1 ring-gray-300 text-sm leading-6', 'placeholder' => 'Title']
+            ])
             ->add('date',DateType::class, [
+                'label' => 'Date',
                 'widget' => 'single_text',
             ])
-            ->add('deposit')
-            ->add('locked')
-            ->add('withdrawal')
-            ->add('interest')
-            ->add('note')
+            ->add('deposit', NumberType::class, [
+                'label' => 'Deposit',
+                'attr' => ['class' => 'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-inset ring-1 ring-gray-300 text-sm leading-6', 'placeholder' => '$0.00']
+            ])
+            ->add('locked', NumberType::class, [
+                'label' => 'Locked',
+                'attr' => ['class' => 'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-inset ring-1 ring-gray-300 text-sm leading-6', 'placeholder' => '$0.00']
+            ])
+            ->add('withdrawal', NumberType::class, [
+                'label' => 'Withdrawal',
+                'attr' => ['class' => 'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-inset ring-1 ring-gray-300 text-sm leading-6', 'placeholder' => '$0.00']
+            ])
+            ->add('interest', NumberType::class, [
+                'label' => 'Interest',
+                'attr' => ['class' => 'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-inset ring-1 ring-gray-300 text-sm leading-6', 'placeholder' => '$0.00']
+            ])
+            ->add('note', TextType::class, [
+                'label' => 'Title',
+                'attr' => ['class' => 'block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-inset ring-1 ring-gray-300 text-sm leading-6', 'placeholder' => 'Note']
+            ])
             ->add('save', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary float-right'
